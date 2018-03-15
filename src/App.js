91f44c59b66ac4a48c1a67168a36a8754beda877
carolina.silvag <carolinasguzman@gmail.com>
2018-03-15 11:14:51 -0300

    App estructura

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
+import { Row, Col, Grid } from 'react-bootstrap'
 import Navbar from './components/navbar/navbar'
+import Search from './components/search/search'
 import './App.css';
 
 class App extends Component {
@@ -9,6 +11,36 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar/>
+        <Grid>
+          <Row className="show-grid SectionBody">
+            <Col xs={12} md={12}>
+              <Row className="show-grid SectionCategories">
+                <Col xs={12} md={12}>
+                  <code></code>
+                </Col>
+              </Row>
+              <Row className="show-grid">
+                <Col xs={3} md={3} className="SectionSidebar">
+                  <code></code>
+                </Col>
+                <Col xs={9} md={9} className="SearchBooks">
+                  <Row className="show-grid Search">
+                    <Col md={12} xs={12}>
+                      <code>
+                        <Search/>
+                      </code>
+                    </Col>
+                  </Row>
+                  <Row className="show-grid Books">
+                    <Col md={12} xs={12}>
+                      <code></code>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>       
+            </Col>
+          </Row>
+        </Grid>;
       </div>
     );
   }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,13 +1,97 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, Button } from 'react-bootstrap'
+
+let url = `https://www.googleapis.com/books/v1/volumes?q=`;
+let name = 'ciencia'
 
 class search extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+  constructor() {
+    super();
+    this.state = {
+      searchData: null
+    };
+  }
+
+  componentWillMount() {
+    this.randomize();
+  }
+
+  randomize() {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse`).then(data => (data.json()))
+    .then(search_data => {
+      console.log(search_data);
+      const SearchData = search_data;
+      /*const SearchData = transformForecast(weather_data);*/
+      console.log(SearchData, 'guardando en data');
+      /*this.setState({forecastData});*/
+    })
+      .catch(error => console.log(error))
+  }
+  /*componentDidMount() {
+    this.updateSearch(this.props.search);
+  }*/
+
+  /*componentWillReceiveProps(nextProps) {
+    if (nextProps.city !== this.props.city) {
+      this.setState({
+        searchData: null,
+      })
+      this.updateBook(nextProps.city);
     }
-}
+  }*/
+
+  updateSearch = () => {
+    const url_search = `ciencia&callback=handleResponse`;
+    fetch(url_search).then(data => (data.json()))
+    .then(search_data => {
+      console.log(search_data);
+      const SearchData = search_data;
+      /*const SearchData = transformForecast(weather_data);*/
+      console.log(SearchData, 'guardando en data');
+      /*this.setState({forecastData});*/
+    });
+  }
+
+  /*renderForcastItemDays(forecastData) {
+    return forecastData.map(forecast => (
+      <ForecastItem key={`${forecast.weekDay}${forecast.hour}`} weekDay = {forecast.weekDay} hour = {forecast.hour} data = {forecast.data}>
+      </ForecastItem>
+    ))
+    // return days.map(day => (<ForecastItem key = {day} weekDay = {day} hour = {10} data = {data}></ForecastItem>))
+      // <ForecastItem weekDay={'Miércoles'}>
+      // </ForecastItem>
+  };
+
+  renderProgress = () => {
+    return (<h3> Cargando pronóstico extendido... </h3>)
+  }
+
+  render() {
+    const {city} = this.props;
+    const {forecastData} = this.state;
+    return(
+    <div>
+      <h2 className="forecastTitle">Pronóstico extendido para {city}</h2>
+      {forecastData !== null ? this.renderForcastItemDays(forecastData) : this.renderProgress()}
+    </div>
+    );
+  }*/
+
+  render() {
+    return(
+      <div>
+        <FormGroup>
+          <FormControl type="text" placeholder="Search" />
+        </FormGroup>{''}
+        <Button type="submit" onClick={this.handleSearch}>Buscar</Button>
+      </div>
+      )
+    }
+  }
+
+/*ForecastExtended.propTypes = {
+  city: PropTypes.string.isRequired,
+}*/
+
 
 export default search;
